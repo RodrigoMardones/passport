528cfd577b9be19558b9823aedc74dadd945eb5d
rodrigo mardones <rignmardones@falabella.cl>
2021-10-15 17:17:00 -0300

    reduccion de interfaces

--- src/domain/interfaces/index.ts
+++ src/domain/interfaces/index.ts
@@ -1,7 +1,3 @@
 export interface UseCase<T, K = unknown> {
-    execute(data?:K): T;
-  }
-
-export interface PromiseUseCase<T, K = unknown> {
-  execute(data?:K): Promise<T>;
+    execute(data?:K): T | Promise<K>;
 }
