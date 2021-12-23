8a641b5156755c66559e3a2bb05464330624e4d8
rodrigo mardones <rignmardones@falabella.cl>
2021-12-23 13:45:45 -0300

    fixes in usecase logic

--- src/application/services/user/user.service.ts
+++ src/application/services/user/user.service.ts
@@ -1,38 +1,44 @@
 import { User, Auth, AuthParams } from "../../../domain/user/models/user.model";
 import IUserRepository from "../../../domain/user/usecases/user.repository";
-import Bcrypt from 'bcrypt';
+import Bcrypt from "bcrypt";
 import DataProvider from "../interfaces/dataProvider";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 export default class UserService implements IUserRepository {
-    constructor(private readonly dataProvider: DataProvider) { }
-    async create(data: User): Promise<User> {
-        const fixedUser: User = {
-            ...data,
-            password: await Bcrypt.hash(data.password, 10),
-        };
-        await this.dataProvider.collection('user').insertOne(data);
-        return fixedUser;
-    }
-    async find(data: AuthParams): Promise<User> {
-        const { email } = data;
-        const query = {
-            email,
-        }
-        return await this.dataProvider.collection('user').findOne(query) as unknown as User;
-    }
-    async validate(hash : string, password: string ): Promise<boolean> {
-        return Bcrypt.compare(password,hash);
-    }
-    async createToken(user: User, _customConfig?:Record<any,any>) : Promise<Auth> {
-        const config = {
-            expiresIn: '2hrs',
-            ..._customConfig,
-        }
-        const token = jwt.sign(user, process.env.SECRET_KEY_JWT as string, config);
-        return {
-            token,
-            ...config,
-            timestamp: new Date().toISOString()
-        };
-    }
+  constructor(private readonly dataProvider: DataProvider) {}
+  async create(data: User): Promise<User> {
+    const hashedPassword = await Bcrypt.hash(data.password, 10);
+    const fixedUser: User = {
+      ...data,
+      password: hashedPassword,
+    };
+    await this.dataProvider.collection("user").insertOne(fixedUser);
+    return fixedUser;
+  }
+  async find(data: AuthParams): Promise<User> {
+    const { email } = data;
+    const query = {
+      email,
+    };
+    return (await this.dataProvider
+      .collection("user")
+      .findOne(query)) as unknown as User;
+  }
+  async validate(hash: string, password: string): Promise<boolean> {
+    return Bcrypt.compare(password, hash);
+  }
+  async createToken(
+    user: User,
+    _customConfig?: Record<any, any>
+  ): Promise<Auth> {
+    const config = {
+      expiresIn: "2hrs",
+      ..._customConfig,
+    };
+    const token = jwt.sign(user, process.env.SECRET_KEY_JWT as string, config);
+    return {
+      token,
+      ...config,
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
